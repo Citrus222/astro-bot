7d48e3552cd334d906bc828751ddeb5e4f577f89
Citrus222 <63605749+Citrus222@users.noreply.github.com>
2026-03-31 20:00:24 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+BOT_TOKEN = “8611829437:AAEn0ZE3O2duVeV0ztKHgYqlPzdvCZPO3X0”
+GEMINI_API_KEY = “AIzaSyB1ZRdCxn4co8C_7XRpR6_SLJRTGyiSqgE”
+
+FREE_REQUESTS = 3  # Бесплатных запросов на пользователя
